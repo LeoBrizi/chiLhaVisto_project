d707f1711008aa836827d7b20432df59ea796673
LeoBrizi <leobrizi.96@gmail.com>
2017-12-16 17:32:22 +0100

    shema0.2

---
+++ app/dataBase_mod.js
@@ -0,0 +1,41 @@
+/*
+modulo per gestire l'interazione con un data base
+*/
+
+var post = require("./post");
+
+var gestoreDB;
+
+//funzione per aggiungere un nuovo post al data base
+var aggiungiPost = function(dataBase,post){
+    console.log(gestoreDB);
+    console.log("classe post funziona nel modulo del database"+post.toString());
+}
+//funzione per rimuovere un post da un data base
+var rimuoviPost = function(dataBase,post){
+
+}
+//funzione per creare un data base vuoto
+var creaDataBase = function(dataBase){
+
+ }
+//funzione per irmuovere un data base
+var eliminaDataBase = function(dataBase){
+
+}
+//funzione per cercare tutti post che coincidono con il postDiPartenza 
+var cerca = function(dataBase,postDiPartenza){
+
+}
+
+var init = function(ip,porta){
+    gestoreDB = [ip,porta];
+}
+
+exports.aggiungiPost = aggiungiPost;
+exports.rimuoviPost = rimuoviPost;
+exports.creaDataBase = creaDataBase;
+exports.eliminaDataBase = eliminaDataBase;
+exports.cerca = cerca;
+exports.init = init;
+
