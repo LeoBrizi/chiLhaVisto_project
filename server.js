1532e349b3a0616609f24763a39f693bcafcd9e6
LeoBrizi <leobrizi.96@gmail.com>
2017-12-17 12:23:48 +0100

    ristrutturazione del maiale

--- server.js
+++ server.js
@@ -2,9 +2,6 @@
 main dell'applicazione, fa il set up del server
 */
 const costanti = require("./costanti.json");
-var db = require("./dataBase_mod");
-db.init(costanti.dataBase.ip,costanti.dataBase.porta);
-var post = require("./post");
 
 var express = require("express");
 var app = express();
@@ -14,10 +11,6 @@ var routes = require('./app/routes');   //importa routes, gestore delle chiamate
 routes(app);
 
 app.listen(port,function(){
-    p = new post.Post(2,2,2,2,2,2,2,2);
-    console.log(p.toString());
-    db.aggiungiPost("34",p);
-    
     console.log("Server running on port "+port);
 })
 
