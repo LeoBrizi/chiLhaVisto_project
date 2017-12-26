6dae97578f1cbec4e8b8642554200bfd09bb9d8c
ariannagavioli <gavioli.arianna@gmail.com>
2017-12-26 14:34:16 +0100

    schema post

---
+++ app/models/listModels.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var PostSchema = new Schema({
+	
+  tipoPost: {
+    type: String,
+    required: 'Per favore, indicare se il post riguarda un oggetto perso o ritrovato'
+  },
+  
+  categoria: {
+	  type: [{
+		  type: String,
+		  enum: ['Elettronica','Abbigliamento','Cartoleria','Altro']
+	  }]
+	  required: "Per favore, indicare a quale categoria appartiene l'oggetto"
+  },
+  
+  sottoCategoria: {
+	  type: String
+  },
+  
+/*  immagine: {
+	  type: 
+  },
+ */
+  
+  data: {
+    type: Date,
+    default: Date.now
+  },
+  
+  luogo: {
+    type: String,
+    required : "Per favore, indicare il luogo dove è stato perso o ritrovato l'oggetto"
+  },
+  
+  ricompensa: {
+	  type: Number
+  },
+  
+  Descrizione: {
+	  type: String
+  }
+});
+
+module.exports = mongoose.model('Post', PostSchema);
