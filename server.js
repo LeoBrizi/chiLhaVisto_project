cadb324f12454094ccfc1da17e2279044a03db3d
EleonoraChiarantano <ele.chiarantano@gmail.com>
2017-12-29 23:30:59 +0100

    modifiche varie

--- server.js
+++ server.js
@@ -10,16 +10,19 @@ var port = process.env.PORT || costanti.defaultPort;
 
 var path = require("path");
 
-var mongoose = require('mongoose');  //va configurato
+var mongoose = require('mongoose');  
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url); // connesso al database
 
 var passport = require('passport');
+require('./config/passport')(passport); // passato passport per configurarlo
+
 var session = require('express-session');
 
 const pug = require('pug');
 
 var bodyParser = require("body-parser"); //per parsare il body della post
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 
 app.set('view engine', 'pug');
@@ -30,8 +33,9 @@ app.use(passport.initialize());
 app.use(passport.session());	//invocato ad ogni richiesta: verifica se lo user è autenticato
 
 var routes = require('./app/routes/listRoutes');   //importa routes, gestore delle chiamate http
-routes(app,passport);
+routes(app,passport); //passati app e passport per essere usati in routes
 
+//LANCIO SERVER-------------------------------------------------------
 app.listen(port,function(){
     console.log("Server running on port "+port);
 })
